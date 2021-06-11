edcabeec35a06346530f1182521d5fc16077af45
agarwalshivani1998 <55907436+agarwalshivani1998@users.noreply.github.com>
2021-06-11 11:39:46 +0530

    Initial commit

---
+++ Virtual Piano/task/src/index.js
@@ -0,0 +1,65 @@
+document.addEventListener("keydown", function (event1) {
+    if (event1.code == "KeyA"){
+        // console.log("The 'A' key is pressed.");
+        let audio = new Audio("Media/A.mp3");
+        audio.play();
+    }
+    else if (event1.code == "KeyS"){
+        // console.log("The 'S' key is pressed.");
+        let audio = new Audio("Media/S.mp3");
+        audio.play();
+    }
+    else if (event1.code == "KeyD"){
+        // console.log("The 'D' key is pressed.");
+        let audio = new Audio("Media/D.mp3");
+        audio.play();
+    }
+    else if (event1.code == "KeyF"){
+        // console.log("The 'F' key is pressed.");
+        let audio = new Audio("Media/F.mp3");
+        audio.play();
+    }
+    else if (event1.code == "KeyG"){
+        // console.log("The 'G' key is pressed.");
+        let audio = new Audio("Media/G.mp3");
+        audio.play();
+    }
+    else if (event1.code == "KeyH"){
+        // console.log("The 'H' key is pressed.");
+        let audio = new Audio("Media/H.mp3");
+        audio.play();
+    }
+    else if (event1.code == "KeyJ"){
+        // console.log("The 'J' key is pressed.");
+        let audio = new Audio("Media/J.mp3");
+        audio.play();
+    }
+    else if (event1.code == "KeyW"){
+        // console.log("The 'W' key is pressed.");
+        let audio = new Audio("Media/W.mp3");
+        audio.play();
+    }
+    else if (event1.code == "KeyE"){
+        // console.log("The 'E' key is pressed.");
+        let audio = new Audio("Media/E.mp3");
+        audio.play();
+    }
+    else if (event1.code == "KeyT"){
+        // console.log("The 'T' key is pressed.");
+        let audio = new Audio("Media/T.mp3");
+        audio.play();
+    }
+    else if (event1.code == "KeyY"){
+        // console.log("The 'Y' key is pressed.");
+        let audio = new Audio("Media/Y.mp3");
+        audio.play();
+    }
+    else if (event1.code == "KeyU"){
+        // console.log("The 'U' key is pressed.");
+        let audio = new Audio("Media/U.mp3");
+        audio.play();
+    }
+    else{
+        // console.log("Error");
+    }
+});
